b0f3f9bd5bd2f6bd278c7f87f5b04f631349b06b
Leonid Dubinsky <dub@podval.org>
2011-10-11 01:01:53 -0400

    Thirds (in addition to degrees, minutes ad seconds).

--- moon/src/main/scala/org/podval/calendar/moon/Angle.scala
+++ moon/src/main/scala/org/podval/calendar/moon/Angle.scala
@@ -17,29 +17,32 @@
 package org.podval.calendar.moon
 
 
-class Angle(val degrees: Int, val minutes: Int, val seconds: Int) {
+class Angle(val degrees: Int, val minutes: Int, val seconds: Int, val thirds: Int) {
 
     if (degrees < 0 || degrees >= 360) throw new IllegalArgumentException()
     if (minutes < 0 || minutes >= 60) throw new IllegalArgumentException()
     if (seconds < 0 || seconds >= 60) throw new IllegalArgumentException()
+    if (thirds < 0 || thirds >= 60) throw new IllegalArgumentException()
 
 
     def +(other: Angle): Angle = Angle(
         degrees + other.degrees,
         minutes + other.minutes,
-        seconds + other.seconds
+        seconds + other.seconds,
+        thirds + other.thirds
     )
 
 
     def *(n: Int): Angle = Angle(
         n*degrees,
         n*minutes,
-        n*seconds
+        n*seconds,
+        n*thirds
     )
 
 
     override def toString: String =
-        degrees + "°" + minutes + "'" + seconds + "\""
+      degrees + "°" + minutes + "′" + seconds + "″" + (if (thirds == 0) "" else thirds + "‴")
 }
 
 
@@ -49,15 +52,26 @@ object Angle {
         apply(degrees, minutes, 0)      
 
 
-    def apply(degrees: Int, minutes: Int, seconds: Int): Angle = {
-        val correctedMinutes = minutes + (seconds / 60)
+    def apply(degrees: Int, minutes: Int, seconds: Int): Angle =
+        apply(degrees, minutes, seconds, 0)      
+
+
+    def apply(degrees: Int, minutes: Int, seconds: Int, thirds: Int): Angle = {
+        val seconds_ = carry(seconds, thirds)
+        val minutes_ = carry(minutes, seconds_)
+        val degrees_ = carry(degrees, minutes_)
 
         new Angle(
-            toRange((degrees + correctedMinutes / 60), 360),
-            toRange(correctedMinutes, 60),
-            toRange(seconds, 60)
+            toRange(degrees_, 360),
+            toRange(minutes_, 60),
+            toRange(seconds_, 60),
+            toRange(thirds, 60)
         )
     }
+
+
+    private def carry(big: Int, small: Int): Int =
+        big + (small / 60)
 
 
     private def toRange(what: Int, range: Int) = {
