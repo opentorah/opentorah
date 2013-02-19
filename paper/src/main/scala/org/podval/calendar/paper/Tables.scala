3343bbc8bc1d428a4199674e5cee19532632d0c6
Leonid Dubinsky <dub@podval.org>
2013-02-18 22:43:31 -0500

    More uniform table generation.

--- paper/src/main/scala/org/podval/calendar/paper/Tables.scala
+++ paper/src/main/scala/org/podval/calendar/paper/Tables.scala
@@ -17,7 +17,7 @@
 package org.podval.calendar.paper
 
 import org.podval.calendar.astronomy.angle.Angle
-import org.podval.calendar.astronomy.{moon, sun}
+import org.podval.calendar.astronomy.{DayData, moon, sun}
 
 import scala.math.{sin, cos, tan, round}
 
@@ -26,63 +26,69 @@ import java.io.File
 
 object Tables {
 
-    private def dayTables(data: Map[Int, Angle]) = {
-        def exactify(days: Int) = Angle.exactify(data(1), days, data(days))
-        def reconstruct(days: Int) = Angle.fromDegrees(exactify(days), 6)
+  private def dayTables(name: String, data: DayData) = {
 
-        val days = new Column("days", "n", (days: Int) => days)
-        val value = new Column("value", "v(n)", (days: Int) => data(days))
-        val calculated = new Column("calculated", "v(1)*n", (days: Int) => data(1)*days)
-        val reconstructed = new Column("reconstructed 1-day movement", "r(n)", reconstruct)
-        val recalculated = new Column("recalculated", "r(10000)*n", (days: Int) => reconstruct(10000)*days)
+    val tables = {
+      val days = Column("days", "n", (days: Int) => days)
+      val value = Column("value", "v(n)", (days: Int) => data.values(days))
+      val calculated = Column("calculated", "v(1)*n", (days: Int) => data.value*days)
+      val reconstructed = Column("reconstructed 1-day movement", "r(n)", (days: Int) => data.exactify(days))
+      val recalculated = Column("recalculated", "r(10000)*n", (days: Int) => data.exact*days)
+      val almagest = Column("Alamgest", "*n", (days: Int) => data.almagest*days)
 
-        List(
-            new Columns("original", days, value),
-            new Columns("calculated", days, value, calculated),
-            new Columns("reconstructed", days, value, reconstructed),
-            new Columns("recalculated", days, value, recalculated)
-        )
+      List(
+        new Columns("original", days, value),
+        new Columns("calculated", days, value, calculated),
+        new Columns("reconstructed", days, value, reconstructed),
+        new Columns("recalculated", days, value, recalculated),
+        new Columns("almagest", days, value, almagest)
+      )
     }
 
-
-    private def mvaTables(data: Map[Angle, Angle]) = {
-        def e(a: Angle, v: Angle) = {
-            val ar = a.toRadians
-            val vr = v.toRadians
-
-            val e = 1/tan(vr)*sin(ar)-cos(ar)
-
-            round(e*100)/100.0
-        }
-
-        val mca = new Column("corrected anomaly", "mca", (a: Angle) => a)
-        val mva = new Column("visible anomaly", "mva", (a: Angle) => data(a))
-        val calculatee = new Column("e", "", (a: Angle) => e(a, data(a)))
-
-        List(
-            new Columns("original", mca, mva),
-            new Columns("withe", mca, mva, calculatee)
-        )
-    }
-    
-
-    private def tables[A,B](name: String, dataList: List[(A, B)], f: (Map[A, B]) => List[Columns[A]]) =
-        f(Map(dataList: _*)) map (_.tabulate(name, dataList map (_._1)))
+    tables map (_.tabulate(name, data.values.map(_._1).toList.sorted))
+  }
 
 
-    private def allTables =
-        tables("sml", sun.LongitudeMean.VALUES, dayTables) ++
-        tables("mml", moon.LongitudeMean.VALUES, dayTables) ++
-        tables("mma", moon.AnomalyMean.VALUES, dayTables) ++
-        tables("mva", sort(moon.AnomalyVisible.CORRECT), mvaTables)
+  private def mvaTables(data: Map[Angle, Angle]) = {
+      def e(a: Angle, v: Angle) = {
+          val ar = a.toRadians
+          val vr = v.toRadians
+
+          val e = 1/tan(vr)*sin(ar)-cos(ar)
+
+          round(e*100)/100.0
+      }
+
+      val mca = new Column("corrected anomaly", "mca", (a: Angle) => a)
+      val mva = new Column("visible anomaly", "mva", (a: Angle) => data(a))
+      val calculatee = new Column("e", "", (a: Angle) => e(a, data(a)))
+
+      List(
+          new Columns("original", mca, mva),
+          new Columns("withe", mca, mva, calculatee)
+      )
+  }
 
 
-    private[this] def sort[A <: Ordered[A], B](map: Map[A, B]): List[(A, B)] = map.toList.sortWith((l, r) => (l._1 < r._1))
+  // XXX do not turn what used to be a Amp into a List and back :)
+  // Unfold sort()
+  private def tables[A,B](name: String, dataList: List[(A, B)], f: (Map[A, B]) => List[Columns[A]]) =
+    f(Map(dataList: _*)) map (_.tabulate(name, dataList map (_._1)))
 
 
-    def main(args: Array[String]) {
-        val directory = new File(if (!args.isEmpty) args(0) else "/tmp/xxx/tables/")
-        directory.mkdirs
-        allTables foreach (_.write(directory))
-    }
+  private def allTables =
+    dayTables("sml", sun.LongitudeMean) ++
+    dayTables("mml", moon.LongitudeMean) ++
+    dayTables("mma", moon.AnomalyMean) ++
+    tables("mva", sort(moon.AnomalyVisible.VALUES), mvaTables)
+
+
+  private[this] def sort[A <: Ordered[A], B](map: Map[A, B]): List[(A, B)] = map.toList.sortWith((l, r) => (l._1 < r._1))
+
+
+  def main(args: Array[String]) {
+    val directory = new File(if (!args.isEmpty) args(0) else "/tmp/xxx/tables/")
+    directory.mkdirs
+    allTables foreach (_.write(directory))
+  }
 }
