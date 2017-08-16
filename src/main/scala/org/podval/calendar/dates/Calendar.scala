4e49d5cf896bac1010ba30a58ccba883d28ded3a
Leonid Dubinsky <dub@podval.org>
2017-08-16 15:05:52 -0400

    Added Travis configuration.

--- src/main/scala/org/podval/calendar/dates/Calendar.scala
+++ src/main/scala/org/podval/calendar/dates/Calendar.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2011-2014 Podval Group.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.podval.calendar.dates
 
 trait Calendar[C <: Calendar[C]] { this: C =>
@@ -31,7 +15,7 @@ trait Calendar[C <: Calendar[C]] { this: C =>
    *
    * @param number  of the Year
    */
-  protected abstract class YearBase(number: Int) extends Numbered[Year](number) { self: Year =>
+  protected abstract class YearBase(number: Int) extends Numbered[Year](number) { this: Year =>
     def character: Year.Character
 
     final def isLeap: Boolean = Year.isLeap(number)
