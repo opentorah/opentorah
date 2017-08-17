a71c21f41d22d7b8f9412042e55cf772fc148235
Leonid Dubinsky <dub@podval.org>
2017-08-17 02:33:22 -0400

    Cleanup.

--- src/main/scala/org/podval/calendar/jewish/Jewish.scala
+++ src/main/scala/org/podval/calendar/jewish/Jewish.scala
@@ -9,9 +9,7 @@ class Jewish private() extends Calendar[Jewish] {
     final override def calendar: Jewish = Jewish.this
   }
 
-  final class Year(number: Int)
-    extends YearBase(number) with JewishCalendarMember
-  {
+  abstract class JewishYear(number: Int) extends YearBase(number) { this: Year =>
     require(0 < number)
 
     // TODO the moment I change the type of newMoon from Moment to Jewish#Moment,
@@ -20,9 +18,9 @@ class Jewish private() extends Calendar[Jewish] {
     //   type mismatch;
     //   found   : Jewish.this.numberSystem.TimeInterval
     //   required: _1.numberSystem.TimeInterval where val _1: org.podval.calendar.jewish.Jewish
-    def newMoon: Moment = month(1).newMoon
+    final def newMoon: Moment = month(1).newMoon
 
-    override def firstDayNumber: Int = {
+    final override def firstDayNumber: Int = {
       val correction =
         if (isAduCorrected) 1
         else if (isFirstCorrected) 1 + (if (isFirstAduCorrected) 1 /* KH 7:3 */ else 0 /* KH 7:2 */)
@@ -33,41 +31,48 @@ class Jewish private() extends Calendar[Jewish] {
       newMoon.day.number + correction
     }
 
-    def isAduCorrected: Boolean = Year.isAdu(newMoon.day)  // KH 7:1
+    final def isAduCorrected: Boolean = Year.isAdu(newMoon.day)  // KH 7:1
 
-    def isFirstCorrected: Boolean = !isAduCorrected && (newMoon.time >= Year.firstCorrection)
+    final def isFirstCorrected: Boolean = !isAduCorrected && (newMoon.time >= Year.firstCorrection)
 
-    def isFirstAduCorrected: Boolean = isFirstCorrected && Year.isAdu(newMoon.day.next)
+    final def isFirstAduCorrected: Boolean = isFirstCorrected && Year.isAdu(newMoon.day.next)
 
-    def isSecondCorrected: Boolean = !isAduCorrected && !isFirstCorrected &&
-      ((newMoon.day.name == DayName.Shlishi) && newMoon.time >= Year.secondCorrection && !this.isLeap)
+    final def isSecondCorrected: Boolean = !isAduCorrected && !isFirstCorrected &&
+      (newMoon.day.name == DayName.Shlishi) && (newMoon.time >= Year.secondCorrection) &&
+      !this.isLeap
 
     // This is not defined for yer 0 - and doesn't apply :)
-    def isThirdCorrected: Boolean = !isAduCorrected && !isFirstCorrected && !isSecondCorrected &&
-      ((newMoon.day.name == DayName.Sheni) && newMoon.time >= Year.thirdCorrection && this.prev.isLeap)
+    final def isThirdCorrected: Boolean = !isAduCorrected && !isFirstCorrected &&
+      !isSecondCorrected &&
+      (newMoon.day.name == DayName.Sheni) && (newMoon.time >= Year.thirdCorrection) &&
+      this.prev.isLeap
 
-    override def lengthInDays: Int = next.firstDayNumber - this.firstDayNumber
+    final override def lengthInDays: Int = next.firstDayNumber - this.firstDayNumber
 
-    def cycle: Int = Year.cycle(number)
+    final def cycle: Int = Year.cycle(number)
 
-    def numberInCycle: Int = Year.numberInCycle(number)
+    final def numberInCycle: Int = Year.numberInCycle(number)
 
-    override def character: YearCharacter = (isLeap, kind)
+    final override def character: YearCharacter = (isLeap, kind)
 
     // KH 8:7,8
-    def kind: YearKind = {
+    final def kind: YearKind = {
       val daysOverShort = lengthInDays - (if (isLeap) 383 else 353)
 
       daysOverShort match {
         case 0 => YearKind.Short
         case 1 => YearKind.Regular
         case 2 => YearKind.Full
-        case _ => throw new IllegalArgumentException("Impossible year length " + lengthInDays + " for " + this)
+        case _ => throw new IllegalArgumentException(
+          "Impossible year length " + lengthInDays + " for " + this)
       }
     }
   }
 
-  final override def createYear(number: Int): Year = new Year(number)
+  final override type Year = JewishYear
+
+  final override def createYear(number: Int): Year =
+    new JewishYear(number) with JewishCalendarMember
 
   final type YearKind = JewishYearKind
 
@@ -80,11 +85,11 @@ class Jewish private() extends Calendar[Jewish] {
     new JewishYearCompanion with JewishCalendarMember
 
   abstract class JewishYearCompanion extends YearCompanion {
-    protected override def characters: Seq[YearCharacter] =
+    protected final override def characters: Seq[YearCharacter] =
       for (isLeap <- Seq(true, false); kind <- YearKind.values) yield (isLeap, kind)
 
     // KH 8:5-6
-    protected override def monthNamesAndLengths(character: YearCharacter): List[MonthNameAndLength] = {
+    protected final override def monthNamesAndLengths(character: YearCharacter): List[MonthNameAndLength] = {
       import MonthName._
 
       character match { case (isLeap: Boolean, kind: YearKind) =>
@@ -112,47 +117,49 @@ class Jewish private() extends Calendar[Jewish] {
 
     private val adu: Set[Jewish#DayName] = Set(DayName.Rishon, DayName.Rvii, DayName.Shishi)
 
-    def isAdu(day: Jewish#Day): Boolean = adu.contains(day.name)
+    final def isAdu(day: Jewish#Day): Boolean = adu.contains(day.name)
 
-    protected override def areYearsPositive: Boolean = true
+    protected final override def areYearsPositive: Boolean = true
 
     private[this] val leapYears: Set[Int] =
       Set(3, 6, 8, 11, 14, 17, 19) // TODO calculate Meton's cycle in the paper
 
-    override def isLeap(yearNumber: Int): Boolean = leapYears.contains(numberInCycle(yearNumber))
+    final override def isLeap(yearNumber: Int): Boolean =
+      leapYears.contains(numberInCycle(yearNumber))
 
-    override def firstMonth(yearNumber: Int): Int = monthsInCycle*(cycle(yearNumber) - 1) + firstMonthInCycle(yearNumber)
+    final override def firstMonth(yearNumber: Int): Int =
+      monthsInCycle*(cycle(yearNumber) - 1) + firstMonthInCycle(yearNumber)
 
-    override def lengthInMonths(yearNumber: Int): Int = lengthInMonths(isLeap(yearNumber))
+    final override def lengthInMonths(yearNumber: Int): Int = lengthInMonths(isLeap(yearNumber))
 
-    def normal: TimeInterval = Month.meanLunarPeriod*lengthInMonths(isLeap = false)
+    final def normal: TimeInterval = Month.meanLunarPeriod*lengthInMonths(isLeap = false)
 
-    def leap: TimeInterval = Month.meanLunarPeriod*lengthInMonths(isLeap = true)
+    final def leap: TimeInterval = Month.meanLunarPeriod*lengthInMonths(isLeap = true)
 
-    def lengthInMonths(isLeap: Boolean): Int = if (isLeap) 13 else 12
+    final def lengthInMonths(isLeap: Boolean): Int = if (isLeap) 13 else 12
 
-    val yearsInCycle: Int = 19
+    final val yearsInCycle: Int = 19
 
-    val leapYearsInCycle: Int = leapYears.size
+    final val leapYearsInCycle: Int = leapYears.size
 
-    val monthsBeforeYearInCycle: Seq[Int] =
+    final val monthsBeforeYearInCycle: Seq[Int] =
       ((1 to yearsInCycle) map lengthInMonths).scanLeft(0)(_ + _)
 
-    val monthsInCycle: Int = monthsBeforeYearInCycle.last
+    final val monthsInCycle: Int = monthsBeforeYearInCycle.last
 
-    val cycleLength: TimeInterval = Month.meanLunarPeriod * monthsInCycle
+    final val cycleLength: TimeInterval = Month.meanLunarPeriod * monthsInCycle
 
-    def firstMonthInCycle(yearNumber: Int): Int =
+    final def firstMonthInCycle(yearNumber: Int): Int =
       monthsBeforeYearInCycle(numberInCycle(yearNumber) - 1) + 1
 
-    def numberInCycle(yearNumber: Int): Int = ((yearNumber - 1) % yearsInCycle) + 1
+    final def numberInCycle(yearNumber: Int): Int = ((yearNumber - 1) % yearsInCycle) + 1
 
-    def cycle(yearNumber: Int): Int = ((yearNumber - 1) / yearsInCycle) + 1
+    final def cycle(yearNumber: Int): Int = ((yearNumber - 1) / yearsInCycle) + 1
 
     // TODO meaningful names
-    val firstCorrection  = interval.hours(18) // KH 7:1
-    val secondCorrection = interval.hours(9).parts(204) // KH 7:4
-    val thirdCorrection  = interval.hours(15).parts(589) // KH 7:5
+    final val firstCorrection  = interval.hours(18) // KH 7:1
+    final val secondCorrection = interval.hours(9).parts(204) // KH 7:4
+    final val thirdCorrection  = interval.hours(15).parts(589) // KH 7:5
   }
 
   final override type Month = JewishMonth
@@ -163,34 +170,39 @@ class Jewish private() extends Calendar[Jewish] {
     // TODO before I can split this out, I need to change return type of the next method to C#Moment;
     // that leads to compilation errors for the 3 corrections - and so do the attempts to
     // split out Calendar.Year etc...
-    def newMoon: Moment = Month.firstNewMoon + Month.meanLunarPeriod*(number-1)
+    final def newMoon: Moment = Month.firstNewMoon + Month.meanLunarPeriod*(number-1)
   }
 
 
   final override type MonthName = JewishMonthName
 
   // TODO stick it into the Month companion???
-  val MonthName: JewishMonthName.type = JewishMonthName
+  final val MonthName: JewishMonthName.type = JewishMonthName
 
   // TODO if this is done with `abstract class` / `final override val`,
   // tests fail (initialization issues?)!
   object Month extends MonthCompanion with JewishCalendarMember {
     // KH 6:3
-    val meanLunarPeriod = interval.days(29).hours(12).parts(793)  // TODO how is this really called? tropical?
+    // TODO how is this really called? tropical?
+    final val meanLunarPeriod = interval.days(29).hours(12).parts(793)
 
-    // Molad of the year of Creation (#1; Man was created on Rosh Hashono of the year #2): BeHaRaD: (KH 6:8)
-    val firstNewMoon = moment.day(2).nightHours(5).parts(204)
+    // Molad of the year of Creation (#1; Man was created on Rosh Hashono of the year #2):
+    // BeHaRaD: (KH 6:8)
+    final val firstNewMoon = moment.day(2).nightHours(5).parts(204)
 
-    override def yearNumber(monthNumber: Int): Int = {
+    final override def yearNumber(monthNumber: Int): Int = {
       val cycleOfMonth = ((monthNumber - 1) / Year.monthsInCycle) + 1
       val yearsBeforeCycle = (cycleOfMonth - 1) * Year.yearsInCycle
-      val yearMonthIsInCycle = Year.monthsBeforeYearInCycle.count(_ < numberInCycleOfMonth(monthNumber))
+      val yearMonthIsInCycle =
+        Year.monthsBeforeYearInCycle.count(_ < numberInCycleOfMonth(monthNumber))
       yearsBeforeCycle + yearMonthIsInCycle
     }
 
-    override def numberInYear(monthNumber: Int): Int = numberInCycleOfMonth(monthNumber) - Year.firstMonthInCycle(yearNumber(monthNumber)) + 1
+    final override def numberInYear(monthNumber: Int): Int =
+      numberInCycleOfMonth(monthNumber) - Year.firstMonthInCycle(yearNumber(monthNumber)) + 1
 
-    private def numberInCycleOfMonth(monthNumber: Int): Int = ((monthNumber - 1) % Year.monthsInCycle) + 1
+    private def numberInCycleOfMonth(monthNumber: Int): Int =
+      ((monthNumber - 1) % Year.monthsInCycle) + 1
   }
 
 
@@ -208,9 +220,9 @@ class Jewish private() extends Calendar[Jewish] {
   abstract class JewishMoment(negative: Boolean, digits: List[Int])
     extends MomentBase(negative, digits)
   {
-    def nightHours(value: Int): Moment = firstHalfHours(value)
+    final def nightHours(value: Int): Moment = firstHalfHours(value)
 
-    def dayHours(value: Int): Moment = secondHalfHours(value)
+    final def dayHours(value: Int): Moment = secondHalfHours(value)
   }
 
   final override type Moment = JewishMoment
