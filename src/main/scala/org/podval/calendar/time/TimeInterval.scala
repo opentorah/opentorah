0ae843385f48cb56d890f1469357fd87c7e3f8e2
Leonid Dubinsky <dub@podval.org>
2017-08-18 15:40:06 -0400

    Cleanup.

--- src/main/scala/org/podval/calendar/time/TimeInterval.scala
+++ src/main/scala/org/podval/calendar/time/TimeInterval.scala
@@ -3,6 +3,8 @@ package org.podval.calendar.time
 import org.podval.calendar.numbers.IntervalBase
 import org.podval.calendar.numbers.NumberSystem.RawNumber
 
+// TODO rename TimeIntervalBase; restore and use Calendar.TimeInterval instead of Interval
+//  (aliases at work :))
 abstract class TimeInterval[S <: TimeNumberSystem[S]](raw: RawNumber)
   extends IntervalBase[S](raw) with TimeNumber[S, S#Interval]
 {
