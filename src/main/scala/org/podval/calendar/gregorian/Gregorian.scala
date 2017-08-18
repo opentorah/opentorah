479e13cc053acc7ca9431b170f86698bca45cb1f
Leonid Dubinsky <dub@podval.org>
2017-08-17 21:15:13 -0400

    Split out YearCompanion from Gregorian.

--- src/main/scala/org/podval/calendar/gregorian/Gregorian.scala
+++ src/main/scala/org/podval/calendar/gregorian/Gregorian.scala
@@ -17,6 +17,7 @@ class Gregorian private() extends Calendar[Gregorian] {
     final override def character: Gregorian#YearCharacter = isLeap
   }
 
+
   final override type Year = GregorianYear
 
   final override def createYear(number: Int): Gregorian#Year =
@@ -26,56 +27,6 @@ class Gregorian private() extends Calendar[Gregorian] {
 
   final override val Year: GregorianYearCompanion =
     new GregorianYearCompanion with GregorianCalendarMember
-
-  abstract class GregorianYearCompanion extends YearCompanion[Gregorian] {
-    protected final override def characters: Seq[Gregorian#YearCharacter] =
-      Seq(true, false)
-
-    protected final override def monthNamesAndLengths(isLeap: Gregorian#YearCharacter):
-      List[Gregorian#MonthNameAndLength] =
-    {
-      def create(name: Gregorian#MonthName, length: Int): Gregorian#MonthNameAndLength =
-        calendar.createMonthNameAndLength(name, length)
-
-      import Gregorian.MonthName._
-      List(
-        create(January  , 31),
-        create(February , if (isLeap) 29 else 28),
-        create(March    , 31),
-        create(April    , 30),
-        create(May      , 31),
-        create(June     , 30),
-        create(July     , 31),
-        create(August   , 31),
-        create(September, 30),
-        create(October  , 31),
-        create(November , 30),
-        create(December , 31)
-      )
-    }
-
-    protected final override def areYearsPositive: Boolean = false
-
-    final override def isLeap(yearNumber: Int): Boolean =
-      (yearNumber % 4 == 0) && ((yearNumber % 100 != 0) || (yearNumber % 400 == 0))
-
-    final override def firstMonth(yearNumber: Int): Int =
-      monthsInYear*(yearNumber - 1) + 1
-
-    final override def lengthInMonths(yearNumber: Int): Int = monthsInYear
-
-    val monthsInYear: Int = 12
-
-    private val daysInNonLeapYear: Int = 365
-
-    final def firstDay(yearNumber: Int): Int =
-      daysInNonLeapYear * (yearNumber - 1) + (yearNumber - 1)/4 - (yearNumber - 1)/100 +
-        (yearNumber - 1)/400 + 1
-
-    final def lengthInDays(yearNumber: Int): Int =
-      if (Year.isLeap(yearNumber)) daysInNonLeapYear + 1 else daysInNonLeapYear
-  }
-
 
   final override type Month = GregorianMonth
 
@@ -87,6 +38,7 @@ class Gregorian private() extends Calendar[Gregorian] {
   // TODO stick it into the Month companion???
   val MonthName: GregorianMonthName.type = GregorianMonthName
 
+
   abstract class GregorianMonthCompanion extends MonthCompanion {
     final override def yearNumber(monthNumber: Int): Int =
       (monthNumber - 1) / Gregorian.Year.monthsInYear + 1
@@ -94,6 +46,7 @@ class Gregorian private() extends Calendar[Gregorian] {
     final override def numberInYear(monthNumber: Int): Int =
       monthNumber - Gregorian.Year.firstMonth(yearNumber(monthNumber)) + 1
   }
+
 
   final override val Month: GregorianMonthCompanion =
     new GregorianMonthCompanion with GregorianCalendarMember
@@ -120,6 +73,7 @@ class Gregorian private() extends Calendar[Gregorian] {
 
     final def afternoonHours(value: Int): Moment = secondHalfHours(value)
   }
+
 
   final override type Moment = GregorianMoment
 
---
+++ src/main/scala/org/podval/calendar/gregorian/GregorianYearCompanion.scala
@@ -0,0 +1,52 @@
+package org.podval.calendar.gregorian
+
+import org.podval.calendar.calendar.YearCompanion
+
+abstract class GregorianYearCompanion extends YearCompanion[Gregorian] {
+  protected final override def characters: Seq[Gregorian#YearCharacter] =
+    Seq(true, false)
+
+  protected final override def monthNamesAndLengths(isLeap: Gregorian#YearCharacter):
+  List[Gregorian#MonthNameAndLength] =
+  {
+    def create(name: Gregorian#MonthName, length: Int): Gregorian#MonthNameAndLength =
+      calendar.createMonthNameAndLength(name, length)
+
+    import Gregorian.MonthName._
+    List(
+      create(January  , 31),
+      create(February , if (isLeap) 29 else 28),
+      create(March    , 31),
+      create(April    , 30),
+      create(May      , 31),
+      create(June     , 30),
+      create(July     , 31),
+      create(August   , 31),
+      create(September, 30),
+      create(October  , 31),
+      create(November , 30),
+      create(December , 31)
+    )
+  }
+
+  protected final override def areYearsPositive: Boolean = false
+
+  final override def isLeap(yearNumber: Int): Boolean =
+    (yearNumber % 4 == 0) && ((yearNumber % 100 != 0) || (yearNumber % 400 == 0))
+
+  final override def firstMonth(yearNumber: Int): Int =
+    monthsInYear*(yearNumber - 1) + 1
+
+  final override def lengthInMonths(yearNumber: Int): Int = monthsInYear
+
+  val monthsInYear: Int = 12
+
+  private val daysInNonLeapYear: Int = 365
+
+  final def firstDay(yearNumber: Int): Int =
+    daysInNonLeapYear * (yearNumber - 1) + (yearNumber - 1)/4 - (yearNumber - 1)/100 +
+      (yearNumber - 1)/400 + 1
+
+  final def lengthInDays(yearNumber: Int): Int =
+    if (calendar.Year.isLeap(yearNumber)) daysInNonLeapYear + 1 else daysInNonLeapYear
+}
