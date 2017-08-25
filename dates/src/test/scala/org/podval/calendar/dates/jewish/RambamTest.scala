212884783bad456c1cef15aaa3961910936607f9
Leonid Dubinsky <dub@podval.org>
2017-08-24 21:09:19 -0400

    Cleanup.

--- dates/src/test/scala/org/podval/calendar/dates/jewish/RambamTest.scala
+++ dates/src/test/scala/org/podval/calendar/dates/jewish/RambamTest.scala
@@ -10,16 +10,12 @@ import Jewish.{Year, Month, Day, interval, week}
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class RambamTest extends FlatSpec {
 
-  "time units" should "be correct" in {
-    val j = Jewish // WTF?!
-
-    // KH 6:2
-    assertResult(24)(hoursPerDay)
+  "time units" should "be as in KH 6:2" in {
+    assertResult(  24)(hoursPerDay)
     assertResult(1080)(partsPerHour)
   }
 
-  "first two years' new moons" should "be correct" in {
-    // KH 6:8
+  "first two years' new moons" should "be as in KH 6:8" in {
     val year1newMoon = Year(1).newMoon
     assertResult(Day.Name.Sheni)(year1newMoon.day.name)
     assertResult(interval.hours(5).parts(204))(year1newMoon.time)
@@ -31,9 +27,8 @@ class RambamTest extends FlatSpec {
     assertResult(year1newMoon)(year2newMoon - Month.meanLunarPeriod*12)
   }
 
-  "molad Nisan example" should "be correct" ignore {
+  "molad Nisan example from KH 6:7" should "be correct" ignore {
     // TODO test fails!
-    // KH 6:7
     val rambamTime = interval.hours(17).parts(107)
 
     val years = for {
@@ -48,30 +43,33 @@ class RambamTest extends FlatSpec {
     // println(years)
   }
 
-  "time intervals" should "be correct" in {
-    val j = Jewish // WTF?!
-
-    // KH 6:3
+  "mean lunar period" should "be as in KH 6:3" in {
     assertResult(interval.days(29).hours(12).parts(793))(Month.meanLunarPeriod)
+  }
 
-    // KH 6:4
+  "year lengths" should "be as in KH 6:4" in {
     assertResult(interval.days(354).hours(8).parts(876))(Year.normal)
-    assertResult(interval.days(365).hours(6))(Sun.yearOfShmuel)
+    assertResult(interval.days(365).hours(6)           )(Sun.yearOfShmuel)
+  }
 
-    // KH 6:5
+  "weekly reminders of month and year" should "be as in KH 6:5" in {
     assertResult(interval.days(1).hours(12).parts(793))(Month.meanLunarPeriod % week)
     assertResult(interval.days(4).hours( 8).parts(876))(Year.normal           % week)
     assertResult(interval.days(5).hours(21).parts(589))(Year.leap             % week)
+  }
 
-    // KH 6:10; 9:1
+  "cycle reminder for year of Shmuel" should "be as in KH 6:10; 9:1" in {
     assertResult(interval.days(365).hours(6))(Sun.yearOfShmuel)
     assertResult(interval.hours(1).parts(485))(Sun.yearOfShmuel*Year.yearsInCycle - Year.cycleLength)
+  }
 
-    // KH 10:1
+  "cycle reminder for year of Rav Ada" should "be as in KH 10:1" in {
     assertResult(interval.days(365).hours(5).parts(997).moments(48))(Sun.yearOfRavAda)
     assertResult(interval)(Sun.yearOfRavAda*Year.yearsInCycle - Year.cycleLength)
   }
 
+  // TODO more tkusfos
+  // TODO reference to KH?
   "tkufos" should "be correct" in {
     assertResult(interval.days(7).hours(9).parts(642))(
       Year(1).month(Month.Name.Nisan).newMoon - Sun.tkufasNissan(1))
