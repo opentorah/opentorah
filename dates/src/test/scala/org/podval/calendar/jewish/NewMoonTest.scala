b625e1e573ab1539757d426af66639be37ff3fec
Leonid Dubinsky <dub@podval.org>
2017-08-28 01:21:01 -0400

    Fixed bug in a test.

--- dates/src/test/scala/org/podval/calendar/jewish/NewMoonTest.scala
+++ dates/src/test/scala/org/podval/calendar/jewish/NewMoonTest.scala
@@ -1,9 +1,9 @@
-package org.podval.calendar.dates
+package org.podval.calendar.jewish
 
 import org.scalatest.FlatSpec
+import org.podval.calendar.dates.Calendar
 import org.podval.calendar.gregorian.Gregorian
-import Gregorian.Month.Name._
-import org.podval.calendar.jewish.Jewish
+import org.podval.calendar.gregorian.Gregorian.Month.Name._
 import Jewish.Day.Name._
 import Jewish.Month.Name._
 
@@ -11,8 +11,6 @@ import Jewish.Month.Name._
 final class NewMoonTest extends FlatSpec {
 
   "new moons from the printed tables" should "calculate correctly" in {
-    // see http://www.owen0001.host-ed.me/cal/moladot.php
-
     // year and month for the molad; jewish date; georgian date; georgian time
     newMoon(5769, Tishrei   , Shlishi ,  5769, Tishrei,  1,  2008, September, 30,  1, 58, 13)
     newMoon(5769, Marheshvan, Rvii    ,  5769, Tishrei, 30,  2008, October  , 29, 14, 42, 14)
@@ -20,16 +18,12 @@ final class NewMoonTest extends FlatSpec {
 
     newMoon(5771, Tishrei   , Chamishi,  5771, Tishrei,  1,  2010, September,  8, 19, 36,  1)
 
+    newMoon(5772, Tishrei   , Shlishi ,  5771, Elul   , 28,  2011, September, 27, 17,  8, 14)
     newMoon(5772, Marheshvan, Chamishi,  5772, Tishrei, 29,  2011, October  , 27,  5, 52, 15)
 
     newMoon(5773, Tishrei   , Rishon  ,  5772, Elul   , 29,  2012, September, 16,  1, 57,  8)
     newMoon(5773, Adar      , Rishon  ,  5773, Shvat  , 30,  2013, February , 10, 17, 37, 13)
     newMoon(5773, Nisan     , Shlishi ,  5773, Nisan  ,  1,  2013, March    , 12,  6, 21, 14)
-  }
-
-  "new moons from the printed tables for Tishrei 5772" should "calculate correctly" ignore {
-    // TODO test fails! Elul 28?! Check the whole year 5771!
-    newMoon(5772, Tishrei   , Shlishi ,  5771, Elul   , 27,  2011, September, 27, 17,  8, 14)
   }
 
   private def newMoon(
