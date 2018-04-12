f01512b139a312be2bac181ae909129ba1e1cbd3
Leonid Dubinsky <dub@podval.org>
2018-04-11 21:35:09 -0400

    Made data directory configurable. Configure both EntityResolver (for XML) and URIResolver (for XSL) (lazily).

--- src/main/scala/org/podval/docbook/gradle/DocBookExtension.scala
+++ src/main/scala/org/podval/docbook/gradle/DocBookExtension.scala
@@ -1,11 +1,16 @@
 package org.podval.docbook.gradle
 
+import java.io.File
+
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 
 class DocBookExtension(project: Project) {
-  private val inputFileName: Property[String] = project.getObjects.property(classOf[String])
+  val inputFileName: Property[String] = project.getObjects.property(classOf[String])
+  def getInputFileName: Property[String] = inputFileName // for Gradle DSL
   inputFileName.set("index")
 
-  def getInputFileName: Property[String] = inputFileName
+  val dataDirectory: Property[File] = project.getObjects.property(classOf[File])
+  def getDataDirectory: Property[File] = dataDirectory // for Gradle DSL
+  dataDirectory.set(DocBookPlugin.buildDirectory(project, "data"))
 }
