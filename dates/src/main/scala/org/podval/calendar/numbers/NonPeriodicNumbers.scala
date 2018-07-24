4f88786eac59bbc46e427a15c18ccff2d63ac289
Leonid Dubinsky <dub@podval.org>
2018-07-24 02:02:22 -0400

    Cleanup.

--- dates/src/main/scala/org/podval/calendar/numbers/NonPeriodicNumbers.scala
+++ dates/src/main/scala/org/podval/calendar/numbers/NonPeriodicNumbers.scala
@@ -2,7 +2,7 @@ package org.podval.calendar.numbers
 
 // TODO introduce NonPeriodicPoint/Vector - or remove NonPeriodicNumbers...
 trait NonPeriodicNumbers[S <: NonPeriodicNumbers[S]] extends Numbers[S] { this: S =>
-  type Point <: PointBase[S]
+  type Point <: PointBase[S] with NonPeriodicNumber[S, Point]
 
-  type Vector <: VectorBase[S]
+  type Vector <: VectorBase[S] with NonPeriodicNumber[S, Point]
 }
