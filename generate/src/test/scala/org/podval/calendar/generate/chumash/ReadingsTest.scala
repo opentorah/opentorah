0188aa6c7f6c22be4e3f8a7a5dd775cec73a8b57
Leonid Dubinsky <dub@podval.org>
2018-08-12 22:43:24 -0400

    Working on the Torah reading schedule (#70). - cleanup.

--- generate/src/test/scala/org/podval/calendar/generate/chumash/ReadingsTest.scala
+++ generate/src/test/scala/org/podval/calendar/generate/chumash/ReadingsTest.scala
@@ -3,14 +3,14 @@ package org.podval.calendar.generate.chumash
 import org.scalatest.{FlatSpec, Matchers}
 import org.podval.calendar.jewish.{Jewish, SpecialDay}
 import Jewish.{Day, Year}
-import Parsha._
+import Parsha.{Tzav, AchareiMot, Metzora, Bemidbar, Naso, Vaetchanan, Nitzavim, Vayelech}
+import SpecialDay.{shabbosAfter, shabbosBefore, Pesach, Shavuot, TishaBav, RoshHashanah}
 
 class ReadingsTest extends FlatSpec with Matchers {
 
   "Torah readings" should "be assigned correctly" in {
     (1 to 6000) foreach { number =>
       val year = Year(number)
-      println(year)
 
       verify(year, inHolyLand = false)
       verify(year, inHolyLand = true)
@@ -23,23 +23,27 @@ class ReadingsTest extends FlatSpec with Matchers {
     def findReadings(day: Day): Readings = readings.find(_._1 == day).get._2
     def isCombined(parsha: Parsha): Boolean = readings.exists(_._2.secondParsha.contains(parsha))
 
-    val readingsBeforePesach: Readings = findReadings(SpecialDay.shabbosBefore(SpecialDay.Pesach(year)))
+    // Pesach
+    val readingsBeforePesach: Readings = findReadings(shabbosBefore(Pesach(year)))
     readingsBeforePesach.isCombined shouldBe false
     readingsBeforePesach.parsha shouldBe (if (!year.isLeap) Tzav else {
-      val roshHaShonohOnChamishi: Boolean = SpecialDay.RoshHashanah(year).name == Day.Name.Chamishi
+      val roshHaShonohOnChamishi: Boolean = RoshHashanah(year).name == Day.Name.Chamishi
       val bothCheshvanAndKislevFullOrLacking = year.kind != Year.Kind.Regular
       if (roshHaShonohOnChamishi && bothCheshvanAndKislevFullOrLacking) AchareiMot else Metzora
     })
 
-    val readingsBeforeShavuot = findReadings(SpecialDay.shabbosBefore(SpecialDay.Shavuot(year)))
+    // Shavuot
+    val readingsBeforeShavuot = findReadings(shabbosBefore(Shavuot(year)))
     readingsBeforeShavuot.isCombined shouldBe false
     val parshaBeforeShavuot = readingsBeforeShavuot.parsha
     (parshaBeforeShavuot == Bemidbar || parshaBeforeShavuot == Naso) shouldBe true
 
-    findReadings(SpecialDay.shabbosAfter(SpecialDay.TishaBav(year))) shouldBe Readings(Vaetchanan)
+    // Tisha Be Av
+    findReadings(shabbosAfter(TishaBav(year))) shouldBe Readings(Vaetchanan)
 
-    findReadings(SpecialDay.shabbosBefore(SpecialDay.RoshHashanah(year+1))).parsha shouldBe Nitzavim
-    val roshHaShanahDay: Day.Name = SpecialDay.RoshHashanah(year+1).name
+    // Rosh Ha Shanah
+    findReadings(shabbosBefore(RoshHashanah(year+1))).parsha shouldBe Nitzavim
+    val roshHaShanahDay: Day.Name = RoshHashanah(year+1).name
     isCombined(Vayelech) shouldBe (roshHaShanahDay != Day.Name.Sheni) && (roshHaShanahDay != Day.Name.Shlishi)
   }
 }
