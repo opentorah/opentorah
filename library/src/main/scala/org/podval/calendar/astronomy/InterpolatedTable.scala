83e78ff70fa89725a9cc4a8e3ae62f5c3cc9dec5
Leonid Dubinsky <dub@podval.org>
2019-01-21 23:41:33 -0500

    Convert TODOs into issues #60: cleanup.

--- library/src/main/scala/org/podval/calendar/astronomy/InterpolatedTable.scala
+++ library/src/main/scala/org/podval/calendar/astronomy/InterpolatedTable.scala
@@ -7,13 +7,15 @@ import org.podval.calendar.numbers.{BigRational, Number}
 trait InterpolatedTable[N <: Number[Angles, N]] {
   val values: Map[N, Rotation]
 
+  private lazy val sortedValues: Seq[(N, Rotation)] = values.toSeq.sortBy(_._1)
+
   // KH 13:7-8, 15:7, KH 16:12
   final def interpolate(argument: N): Rotation = {
-    // TODO presort to avoid constant sorting...
-    val (before /*: N*/, beforeValue: Rotation) = values.filter(_._1 <= argument).toList.maxBy(_._1)
+    val (allBefore: Seq[(N, Rotation)], allAfter: Seq[(N, Rotation)]) = sortedValues.span(_._1 <= argument)
+    val (before /*: N*/, beforeValue: Rotation) = allBefore.last
     val reminder: Rotation = argument - before
     val more = if (reminder.isZero) Rotation.zero else {
-      val (after /*: N*/, afterValue : Rotation) = values.filter(_._1 >  argument).toList.minBy(_._1)
+      val (after /*: N*/, afterValue : Rotation) = allAfter.head
       val change: Rotation = afterValue - beforeValue
       val span: Rotation = after - before
       val portion: BigRational = reminder.toRational/span.toRational
