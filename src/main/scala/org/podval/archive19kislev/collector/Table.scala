1437015389048ab9ef3c7f9dc39f048f30229525
Leonid Dubinsky <dub@podval.org>
2019-04-07 23:51:08 -0400

    - CSS cleanup (#26);

--- src/main/scala/org/podval/archive19kislev/collector/Table.scala
+++ src/main/scala/org/podval/archive19kislev/collector/Table.scala
@@ -5,10 +5,10 @@ import scala.xml.{Elem, Node}
 final class Table[D](preRow: D => Seq[Node], columns: Column[D]*) {
   def toTei(data: Seq[D]): Elem =
     <table rendition="collection-index">
-      <row>{ for (column <- columns) yield <cell>{column.heading}</cell> }</row>
+      <row>{ for (column <- columns) yield <cell rendition={column.cssClass}>{column.heading}</cell> }</row>
       { data.flatMap { rowData =>
       preRow(rowData).flatMap(line => <row><cell cols={columns.length.toString}>{line}</cell></row>) ++
-      <row>{for (column <- columns) yield <cell>{column.value(rowData)}</cell>}</row>
+      <row>{for (column <- columns) yield <cell rendition={column.cssClass}>{column.value(rowData)}</cell>}</row>
       }}
     </table>
 }
