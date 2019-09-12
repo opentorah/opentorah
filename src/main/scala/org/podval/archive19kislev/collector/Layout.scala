45ee1bbaa032112a3d79bd09871de2e71f211c19
Leonid Dubinsky <dub@podval.org>
2019-09-12 13:42:14 -0400

    - moved all collections and `names` under `collections`; - renamed `archive` to `rgada`; - fixed up cross-references; - only URLs without leading slash make it into the navigation bar   when listed in `_config.yml`;

--- src/main/scala/org/podval/archive19kislev/collector/Layout.scala
+++ src/main/scala/org/podval/archive19kislev/collector/Layout.scala
@@ -3,27 +3,57 @@ package org.podval.archive19kislev.collector
 import java.io.File
 
 object Layout {
-  final val docsRoot: File = new File("docs").getAbsoluteFile
 
-  def docs(fileName: String): File = new File(docsRoot, fileName)
+  private final val docs: File = new File("docs").getAbsoluteFile
+
+  private def docs(fileName: String): File = new File(docs, fileName)
+
+  final val indexMd: File = docs("index.md")
+
+  final val configYml: File = docs("_config.yml")
+
+  // Note: also hard-coded in _layouts/tei.html (for 'names') and in 'index.md'!
+  private final val collectionsDirectoryName: String = "collections"
+
+  final val collections: File = docs(collectionsDirectoryName)
+
+  def collections(name: String): File = new File(collections, name)
+
+  final val collectionsXml: File = collections("collections.xml")
+
+  private def url(ref: String): String = s"/$collectionsDirectoryName/$ref"
+
+  final val namesFileDirectory: File = collections
 
   final val namesFileName: String = "names"
 
-  final val collectionsXmlFileName: String = "collections.xml"
+  final val namesUrl: String = url(s"$namesFileName.html")
 
-  final val collectionXmlFileName: String = "collection.xml"
+  def collectionXml(directory: File): File = new File(directory, "collection.xml")
 
-  final val indexMdFileName: String = "index.md"
+  def collectionUrl(name: String): String = url(s"$name/index.html")
 
-  final val configYmlFileName: String = "_config.yml"
+  // Note: also hard-coded in _layouts/facsimile.html!
+  private final val facsimilesDirectoryName: String = "facsimiles"
 
-  object Collection {
-    final val teiDirectoryName: String = "tei"
+  def facsimiles(collectionDirectory: File): File = new File(collectionDirectory, facsDirectoryName)
 
-    final val facsimilesDirectoryName: String = "facsimiles"
+  // Note: also hard-coded in _layouts/tei.html!
+  final val facsDirectoryName: String = "facs" // facsimile viewers
 
-    final val docsDirectoryName: String = "documents" // wrappers for TEI XML
+  def facs(collectionDirectory: File): File = new File(collectionDirectory, facsDirectoryName)
 
-    final val facsDirectoryName: String = "facs" // facsimile viewers
-  }
+  final val teiDirectoryName: String = "tei"
+
+  def tei(collectionDirectory: File): File = new File(collectionDirectory, teiDirectoryName)
+
+  // Note: also hard-coded in _layouts/facsimile.html!
+  final val docsDirectoryName: String = "documents" // wrappers for TEI XML
+
+  def docs(collectionDirectory: File): File = new File(collectionDirectory, docsDirectoryName)
+
+  def documentUrl(collectionDirectoryName: String, name: String): String =
+    url(s"$collectionDirectoryName/${documentUrlRelativeToIndex(name)}")
+
+  def documentUrlRelativeToIndex(name: String): String =  s"$docsDirectoryName/$name.html"
 }
