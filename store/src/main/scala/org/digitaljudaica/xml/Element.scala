79b7f3bbdb0337c5c1dffdc2d00c0030a9c80bf3
Leonid Dubinsky <dub@podval.org>
2020-02-12 04:14:19 -0500

    - cleanup;

--- store/src/main/scala/org/digitaljudaica/xml/Element.scala
+++ store/src/main/scala/org/digitaljudaica/xml/Element.scala
@@ -2,7 +2,47 @@ package org.digitaljudaica.xml
 
 import cats.implicits._
 
-object Element extends ElementMethods(charactersAllowed = false) {
+class Element(charactersAllowed: Boolean) {
+  object optional {
+    final def apply[A](name: String, parser: Parser[A]): Parser[Option[A]] =
+      apply(name = Some(name), parser)
+
+    final def apply[A](parser: Parser[A]): Parser[Option[A]] =
+      apply(name = None, parser)
+
+    final def apply[A](name: Option[String], parser: Parser[A]): Parser[Option[A]] = for {
+      noElement <- Element.nextNested.nameIsNot(name)
+      result <- if (noElement) Parser.pure(None) else for {
+        next <- Context.takeNextNestedElement
+        result <- Context.nested(None, next, parser, charactersAllowed)
+      } yield Some(result)
+    } yield result
+  }
+
+  object required {
+    final def apply[A](parser: Parser[A]): Parser[A] =
+      Parser.required(s"element", optional(parser))
+
+    final def apply[A](name: String, parser: Parser[A]): Parser[A] =
+      Parser.required(s"element '$name'", optional(name, parser))
+  }
+
+  object all {
+    final def apply[A](name: String, parser: Parser[A]): Parser[Seq[A]] =
+      apply(Some(name), parser)
+
+    final def apply[A](parser: Parser[A]): Parser[Seq[A]] =
+      apply(None, parser)
+
+    final def apply[A](name: Option[String], parser: Parser[A]): Parser[Seq[A]] = for {
+      headOption <- optional(name, parser)
+      tail <- if (headOption.isEmpty) Parser.pure(Seq.empty[A]) else apply(name, parser)
+      result = headOption.toSeq ++ tail
+    } yield result
+  }
+}
+
+object Element extends Element(charactersAllowed = false) {
 
   val name: Parser[String] = Context.getName
 
@@ -19,5 +59,5 @@ object Element extends ElementMethods(charactersAllowed = false) {
     } yield nextNestedElementName.isEmpty || expected.fold(false)(expected => !nextNestedElementName.contains(expected))
   }
 
-  object withCharacters extends ElementMethods(charactersAllowed = true)
+  object withCharacters extends Element(charactersAllowed = true)
 }
