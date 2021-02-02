82f57f0106a22397089653c2307dcf20799ecdff
Leonid Dubinsky <dub@podval.org>
2021-02-02 18:56:41 -0500

    0.2.3 (#228)
    - TEI link resolving cleanup and a fix for the absolute links;

--- xml/src/main/scala/org/opentorah/xml/LinkResolver.scala
+++ xml/src/main/scala/org/opentorah/xml/LinkResolver.scala
@@ -1,15 +1,6 @@
 package org.opentorah.xml
 
-import org.opentorah.util.Files
-
 trait LinkResolver {
-  // TODO maybe just call up?
-  final def resolve(target: String): Option[Html.a] = if (!target.startsWith("/")) None else {
-    val (url: String, part: Option[String]) = Files.urlAndPart(target)
-    resolve(Files.splitUrl(url))
-      .map(a => a.copy(part = part))
-  }
-
   def resolve(url: Seq[String]): Option[Html.a]
 
   def findByRef(ref: String): Option[Html.a]
